04b05023abf3005fc6d80d716ac4479092990304
Jelson <jelsoncharley@gmail.com>
2019-09-07 17:02:15 +0530

    Initial Commit

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,19 @@
+return {
+  ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
+  ["workshop-1852257480"]={ configuration_options={ horizontal=570, vertical=-30 }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-543945797"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="waving",
+      dmg_only="on",
+      number_size="default",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-770901818"]={
+    configuration_options={ days=2, enable_houndattack=true, format="complex" },
+    enabled=true 
+  } 
+}
