5c3e2d8712953190bc1c206f6c3b2941fb67d3da
Jelson <jelsoncharley@gmail.com>
2019-10-29 14:45:49 +0530

    Day 1117

--- Caves/modoverrides.lua
+++ Caves/modoverrides.lua
@@ -1,14 +1,5 @@
 return {
   ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
-  ["workshop-1842021927"]={
-    configuration_options={
-      OptionCheckingForFollowers=0.5,
-      OptionHUDdown=0,
-      OptionHUDhorizontal=0,
-      OptionMaxFollowers=6 
-    },
-    enabled=true 
-  },
   ["workshop-1852257480"]={ configuration_options={ horizontal=570, vertical=-30 }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
   ["workshop-543945797"]={
